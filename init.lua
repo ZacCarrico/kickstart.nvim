f07fa0f32819de63124bea076af5db9ec2cbb6d4
Zac Carrico <zac.carrico@apella.io>
2025-11-03 16:45:08 -0600

    yy saves to clipboard on mac

--- init.lua
+++ init.lua
@@ -111,12 +111,9 @@ vim.o.mouse = 'a'
 vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
+--  This makes all yank/delete operations use the system clipboard
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
